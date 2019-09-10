43a1fed75dcb3cb930318ef1c030526978f4af23
darkbeast69 <guptaaryan189@gmail.com>
2019-09-10 06:14:42 +0000

    jasmine: : Refactor BoardConfigQcom to ProductConfigQcom

--- device.mk
+++ device.mk
@@ -130,6 +130,10 @@ PRODUCT_COPY_FILES += \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     $(TOPDIR)frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# Board
+PRODUCT_USES_QCOM_HARDWARE := true
+PRODUCT_BOARD_PLATFORM := msm8998
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbthost_if
